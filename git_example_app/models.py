9b84ed854f5476ce1ff6f7c0a680511719369598
joshcodingdojo <jgendal@codingdojo.com>
2021-06-01 18:54:13 -0400

    added post model

--- git_example_app/models.py
+++ git_example_app/models.py
@@ -5,3 +5,7 @@ class User(models.Model):
   first_name = models.CharField(max_length=45)
   last_name = models.CharField(max_length=45)
   age = models.IntegerField()
+
+class Post(models.Model):
+  text = models.TextField()
+  user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
